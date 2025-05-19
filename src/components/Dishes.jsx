8d89273c25c4c03adb1fc29e0f9dbcf92922f4ed
lakshitasethi2703 <lakshita.sethi@deeporion.com>
2025-05-19 12:45:15 +0530

    add chnages

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -5,18 +5,23 @@ import img3 from "../assets/img/img3.jpg";
 import img4 from "../assets/img/img4.jpg";
 import img5 from "../assets/img/img5.jpg";
 import img6 from "../assets/img/img6.jpg";
+import drink from "../assets/img/drink.png";
+import coldcoffee from "../assets/img/cold-coffee.png";
+import chocolateShake from "../assets/img/chocolate-shake.png";
 import DishesCard from "../layouts/DishesCard";
 
 const Dishes = () => {
   return (
-    <div className=" min-h-screen flex flex-col justify-center items-center lg:px-32 px-5">
+    <div className=" min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 " >
       <h1 className=" text-4xl font-semibold text-center pt-24 pb-10">
-        Our Dishes
+        Our Drinks 
+        <img src={drink} alt="Drink Icon" className="inline-block w-8 h-8 gap-2 ml-2" />
+
       </h1>
 
       <div className=" flex flex-wrap gap-8 justify-center">
-        <DishesCard img={img1} title="Tasty Dish" price="100" />
-        <DishesCard img={img2} title="asty Dish" price="200" />
+        <DishesCard img={coldcoffee} title="Cold Coffee" price="200" />
+        <DishesCard img={chocolateShake} title="Chocolate Shake" price="250" />
         <DishesCard img={img3} title="fasty Dish" price="150" />
         <DishesCard img={img4} title="hasty Dish" price="450" />
         <DishesCard img={img5} title="hasty Dish" price="500" />
--- src/layouts/DishesCard.jsx
+++ src/layouts/DishesCard.jsx
@@ -1,32 +1,3 @@
-// import React from "react";
-// import { BsStarFill } from "react-icons/bs";
-// import { BsStarHalf } from "react-icons/bs";
-// import Button from "../layouts/Button";
-
-// const DishesCard = (props) => {
-//   return (
-//     <div className=" w-full lg:w-1/4 p-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg">
-//       <img className=" rounded-xl" src={props.img} alt="img" />
-//       <div className=" space-y-4">
-//         <h3 className=" font-semibold text-center text-xl pt-6">{props.title}</h3>
-//         <div className=" flex flex-row justify-center">
-//           <BsStarFill className=" text-brightColor" />
-//           <BsStarFill className=" text-brightColor" />
-//           <BsStarFill className=" text-brightColor" />
-//           <BsStarFill className=" text-brightColor" />
-//           <BsStarHalf className=" text-brightColor" />
-//         </div>
-//         <div className=" flex flex-row items-center justify-center gap-4">
-//           <h3 className=" font-semibold text-lg">{props.price}</h3>
-//           <Button title="Buy Now" />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DishesCard;
-
 
 import React from "react";
 import { useCart } from "../context/CartContext";
@@ -40,9 +11,9 @@ const DishesCard = ({ img, title, price }) => {
 
   return (
     <div className="w-64 bg-white p-4 rounded-xl shadow">
-      <img src={img} alt={title} className="w-full h-40 object-cover rounded" />
+      <img src={img} alt={title} className="w-full h-25 object-cover rounded" />
       <h2 className="font-semibold mt-3 text-lg">{title}</h2>
-      <p className="text-orange-500 mt-1">⭐️⭐️⭐️⭐️☆</p>
+      {/* <p className="text-orange-500 mt-1">⭐️⭐️⭐️⭐️☆</p> */}
       <div className="flex justify-between items-center mt-3">
         <span className="font-bold">{price}</span>
         <button
